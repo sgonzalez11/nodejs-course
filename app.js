8820b53d7b72a2107c01a64eaa0da9be2e4712be
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-07-04 22:37:01 -0300

    database test connection added and commented

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -18,6 +19,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+// db.execute('SELECT * FROM products')
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
