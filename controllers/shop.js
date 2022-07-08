768764073ef2ad5cd7f12e9728b0bf34beb39149
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-07-07 23:24:11 -0300

    more advance using magic methods of sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,26 +39,31 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    Cart.getCart(cart => {
-        Product.fetchAll(products => {
-            const cartProducts = [];
-            for (product of products) {
-                const cartProductData = cart.products.find(prod => prod.id === product.id);
-                if (cartProductData) {
-                    cartProducts.push({productData: product, qty: cartProductData.qty});
-                }
-            }
-            res.render('shop/cart', {
-                path: '/cart',
-                pageTitle: 'Your Cart',
-                products: cartProducts
-            });
-        });
-    });
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart
+                .getProducts()
+                .then(products =>{
+                    res.render('shop/cart', {
+                        path: '/cart',
+                        pageTitle: 'Your Cart',
+                        products: products
+                    });
+                })
+                .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
+    // req.user
+    //     .getCart()
+    //     .then(cart => {
+    //
+    //     })
+    //     .catch(err => console.log(err));
     Product.findById(prodId, (product) => {
         Cart.addProduct(prodId, product.price);
     });
