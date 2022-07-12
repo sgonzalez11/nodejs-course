f24dbd9cc73e02c8092b5bcbfc98010e1123373a
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-07-11 22:43:49 -0300

    Add existing products and retrieving from cart

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/products');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -20,28 +24,52 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// db.execute('SELECT * FROM products')
-//     .then(result => {
-//         console.log(result);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404Page);
 
-sequelize.sync()
+// One User can may have a lot of Products
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+// Un Usuario puede tener un carrito
+User.hasOne(Cart);
+Cart.belongsTo(User);
+// Un carrito puede tener mucho productos y un producto estar en muchos carritos
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+
+sequelize
+    .sync()
+    //.sync({force: true})
     .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            User.create({ name: 'Seb', email: 'sgonzalez11@gmail.com'});
+        }
+        return user;
+    })
+    .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
         console.log(err);
     });
-
-
--- controllers/shop.js
+++ controllers/shop.js
@@ -44,7 +44,7 @@ exports.getCart = (req, res, next) => {
         .then(cart => {
             return cart
                 .getProducts()
-                .then(products =>{
+                .then(products => {
                     res.render('shop/cart', {
                         path: '/cart',
                         pageTitle: 'Your Cart',
@@ -58,16 +58,36 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    // req.user
-    //     .getCart()
-    //     .then(cart => {
-    //
-    //     })
-    //     .catch(err => console.log(err));
-    Product.findById(prodId, (product) => {
-        Cart.addProduct(prodId, product.price);
-    });
-    res.redirect('/cart');
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user
+        // Obtain the cart
+        .getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts({where: {id: prodId}});
+        })
+        .then(products => {
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+            }
+            if (product) {
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product;
+            }
+            return Product.findByPk(prodId);
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, {
+                through: {quantity: newQuantity}
+            });
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
