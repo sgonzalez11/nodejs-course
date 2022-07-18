ad070109ed0fa922185c0e682a8a008fa6872fc2
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-07-17 22:51:25 -0300

    create orders and orders items

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/products');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -50,11 +52,14 @@ Cart.belongsTo(User);
 // Un carrito puede tener mucho productos y un producto estar en muchos carritos
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
-
+//
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem})
 
 sequelize
     .sync()
-    //.sync({force: true})
+    // .sync({force: true})
     .then(result => {
         return User.findByPk(1);
     })
