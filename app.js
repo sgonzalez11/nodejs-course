0de31e10f43fef6f3e01d00ac7051312f6c872b5
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-08-15 21:08:27 -0300

    setting up, installing, creating connection of mongo db

--- app.js
+++ app.js
@@ -2,79 +2,35 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/products');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database');
 
 const app = express();
 
-// app.engine(
-//     'hbs',
-//     expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch(err => console.log(err));
 })
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404Page);
 
-// One User can may have a lot of Products
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-// Un Usuario puede tener un carrito
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// Un carrito puede tener mucho productos y un producto estar en muchos carritos
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-//
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem})
-
-sequelize
-    .sync()
-    // .sync({force: true})
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            User.create({ name: 'Seb', email: 'sgonzalez11@gmail.com'});
-        }
-        return user;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+mongoConnect(() => {
+    app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('nodecourse', 'root', 'Lucecita0510', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = () => {
+    MongoClient
+        .connect('mongodb+srv://sebastian:Lucecita0510@cluster0.eyvlq.mongodb.net/?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected');
+            _db = client.db();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        })
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No Database found!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb
