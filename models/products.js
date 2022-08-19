c559e12d5915ba51a161bf508923668760dc1d1d
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-08-19 10:43:46 -0300

    fetching single product

--- models/products.js
+++ models/products.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -25,6 +26,20 @@ class Product {
         return db.collection('products')
             .find()
             .toArray()
+            .then(products => {
+                console.log(products);
+                return products;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(prodId) {
+        const db = getDb();
+        return db.collection('products')
+            .find({_id: new mongodb.ObjectId(prodId)})
+            .next()
             .then(products => {
                 console.log(products);
                 return products;
