8bcb6049d7fa5ef94006343e823a8c347906d186
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-08-21 21:57:51 -0300

    Deleting a single product

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,12 +85,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-        .then(product => {
-            return product.destroy();
-        })
-        .then(result => {
-            console.log('DELETED PRODUCT');
+    Product.deleteById(prodId)
+        .then(() => {
             res.redirect('/admin/products');
         })
         .catch(err => console.log(err));
