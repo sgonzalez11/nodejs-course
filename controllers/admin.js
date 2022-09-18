8d14c0e6fd4fe97b51930a0766dc63a7cdac4f54
Sebastian Rafael Gonzalez <sebastian.gonzalez@darwoft.com>
2022-09-18 11:10:52 -0300

    Usings Relations in Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,13 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
     const imageUrl = req.body.imageUrl;
-    const product = new Product({title: title, price: price, description: description, imageUrl: imageUrl});
+    const product = new Product({
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+        userId: req.user
+    });
     product
         .save()
         .then(result => {
